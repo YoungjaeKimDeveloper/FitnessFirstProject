0bad18630c2fe29ae55ebb51941e81f364b3338b
Youngjae <2015fromjeki@gmail.com>
2024-09-18 11:57:13 +1000

    Process of Creating Modal

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header/Header";
 import HeroComponent from "../components/Hero/HeroComponent";
 import FitnessProgramme from "../components/FitnessProgrammeLayout/FitnessProgrammeLayout";
 import Footer from "../components/Footer/Footer";
+import Modal from "../components/Modal/Modal";
 
 const HomePage = () => {
+  const [modal, setModal] = useState(true);
+  const toggleModal = () => setModal((prev) => !prev);
   return (
     <div>
-      <HeroComponent />
-      <FitnessProgramme />
-      <Footer />
+      {modal ? (
+        <Modal />
+      ) : (
+        <>
+          <HeroComponent />
+          <FitnessProgramme />
+          <Footer />
+        </>
+      )}
     </div>
   );
 };

c18a9156cc974df5acf57e5a8199a4316f9770fd
Youngjae <2015fromjeki@gmail.com>
2024-09-23 10:46:22 +1000

    Before implementing the table Design

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,14 +3,22 @@ import "./CartPage.css";
 import foods from "../../json/foods.json";
 const CartPage = () => {
   const shoppingLists = ["ProtenBar1", "ProtenBar2", "ProtenBar3"];
+
   return (
     <div className="cartPage-layout">
       <div className="cartPage-Container">
         <h1>Cart List</h1>
+
         {foods.slice(0, 5).map((shoppingList, index) => (
           <>
-            <li key={index}>{shoppingList.foodTitle}</li>
-            <img src={shoppingList.img} alt="alt-img" className="cartPage-alt-img"/>
+            <li key={index} className="food-item">
+              {index + 1}.{shoppingList.foodTitle}
+              <img
+                src={shoppingList.img}
+                alt="alt-img"
+                className="food-item-img"
+              />
+            </li>
           </>
         ))}
       </div>

a9f42a5432f61c4b6d7a0fb0a4c19a9957bd13ac
Youngjae <2015fromjeki@gmail.com>
2024-09-23 11:40:31 +1000

    Add hardcoding cart Page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import "./CartPage.css";
 import foods from "../../json/foods.json";
 const CartPage = () => {
-  const shoppingLists = ["ProtenBar1", "ProtenBar2", "ProtenBar3"];
-
+  const tempFoodsLists = foods.slice(0, 5);
+  const calTotalPrice = (prev, next) => prev + next.price;
+  const total = tempFoodsLists.reduce(calTotalPrice, 0);
   return (
     <div className="cartPage-layout">
       <div className="cartPage-Container">
         <h1>Cart List</h1>
+        <table className="cart-list-table">
+          {/* Table Head Part */}
+          <tr>
+            <th>No</th>
+            <th>ItemName</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+          {tempFoodsLists.map((shoppingList, index) => (
+            <>
+              <tr>
+                <td>{index + 1}</td>
+                <td>{shoppingList.foodTitle}</td>
+                <td>1</td>
+                <td>${shoppingList.price}</td>
+              </tr>
+            </>
+          ))}
 
-        {foods.slice(0, 5).map((shoppingList, index) => (
+          <tr className="total">
+            <td className="total"></td>
+            <td className="total"></td>
+            <td>40</td>
+            <td>${total}</td>
+          </tr>
+        </table>
+        {/* {foods.slice(0, 5).map((shoppingList, index) => (
           <>
             <li key={index} className="food-item">
               {index + 1}.{shoppingList.foodTitle}
@@ -20,7 +46,7 @@ const CartPage = () => {
               />
             </li>
           </>
-        ))}
+        ))} */}
       </div>
     </div>
   );

99fdbae18dafca26988008513697af2e575555fa
Youngjae <2015fromjeki@gmail.com>
2024-09-25 11:52:24 +1000

    Add News API

--- src/components/Blog/BlogNews.jsx
+++ src/components/Blog/BlogNews.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
-
-import "./BlogNews.css";
-import newImg from "../../../public/assets/boxing.jpg";
+import axios from "axios";
 import BlogNewsElement from "./BlogNewsElement";
+import "./BlogNews.css";
 const NewsLayout = () => {
   const [newsAPI, setNewsAPI] = useState([]);
+  const [error, setError] = useState("");
+  const fetchNews = async () => {
+    try {
+      const response = axios.get(
+        "https://gnews.io/api/v4/search?q=example&apikey=ffe226945c502b0f15ec9953fff847a5"
+      );
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   useEffect(() => {}, []);
   return (
     <div className="blog-news">

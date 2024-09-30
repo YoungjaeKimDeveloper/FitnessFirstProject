2c5f82a83b866d6664a9d957cb206b32926893f6
Youngjae <2015fromjeki@gmail.com>
2024-09-30 21:53:33 +1000

    Added Basic CSS style to writing Page

--- src/pages/WritingPage.jsx
+++ src/pages/WritingPage.jsx
@@ -15,35 +15,49 @@ const WritingPage = () => {
           <img src={profilePic} alt="profile-pic" className="profile-pic" />
         </div>
       </div>
-      <div className="writing-right-contents">
-        <label for="writing-title">
-          Title
-          <input id="title" type="text" className="writing-title" />
-        </label>
-        <label for="content">
-          Content
-          <input id="content" type="text" className="writing-content" />
-        </label>
-        <label for="date">
-          Date
-          <input id="date" type="date" className="writing-content" />
-        </label>
-        <lable for="happy">
-          Happy
-          <input type="radio" value="happy" id="happy" name="mood" />
-        </lable>
-        <lable for="sad">
-          Sad
-          <input type="radio" value="sad" id="sad" name="mood" />
-        </lable>
-        <lable for="happy">
-          Happy
-          <input type="radio" value="happy" id="happy" name="mood" />
-        </lable>
-        <label htmlFor="">
-          Upload Image
-          <input type="file" />
-        </label>
+      <div className="writing-right-contents-layout">
+        <div className="right-contents-container">
+          <label for="writing-title" className="writing-input">
+            Title
+            <input
+              id="title"
+              type="text"
+              className="writing-title diary-input"
+              placeholder="Write Your title.."
+            />
+          </label>
+          <label for="content" className="writing-input">
+            Content
+            <input
+              id="content"
+              type="text"
+              className="writing-content diary-input"
+              placeholder="Share your story..."
+            />
+          </label>
+          <label for="date" className="writing-input">
+            Date
+            <input id="date" type="date" className="writing-content" />
+          </label>
+          <div className="emotion-selections">
+            <lable for="happy" className="emotion">
+              Happy
+              <input type="radio" value="happy" id="happy" name="mood" />
+            </lable>
+            <lable for="sad" className="emotion">
+              Sad
+              <input type="radio" value="sad" id="sad" name="mood" />
+            </lable>
+            <lable for="happy" className="emotion">
+              Bad
+              <input type="radio" value="happy" id="happy" name="mood" />
+            </lable>
+          </div>
+          <label htmlFor="" className="writing-input">
+            Upload Image
+            <input type="file" />
+          </label>
+        </div>
       </div>
     </div>
   );
